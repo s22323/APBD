c0e0f468877a69313640ff17472569bff7f0540b
Marta Peska <s22323@pjwstk.edu.pl>
2022-10-15 16:20:11 +0200

    Zadanie 3: Pierwsza wersja Crawlera

---
+++ APBD1/Crawler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace APBD1
+{
+   class Crawler
+    {
+        private static void showEmails(string text)
+        {
+            String pattern = @"\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*";
+            MatchCollection matchCollection = Regex.Matches(text, pattern);
+            ArrayList list = new ArrayList();
+            foreach (Match match in matchCollection)
+            {
+                list.Add(match.Value);
+            }
+                foreach (String s in list)
+                {
+                Console.WriteLine(s);
+                }
+        }
+        static async Task Main(string[] args)
+        {
+            string websiteUrl = args[0];
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage response = await httpClient.GetAsync(websiteUrl);
+            string websiteContent = await response.Content.ReadAsStringAsync();
+            showEmails(websiteContent);
+        }
+    }
+}
